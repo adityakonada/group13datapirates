14af0cedfb0068124a6c3e812aecf83fb0d86e45
aditya <adityakonada@yahoo.com>
2019-03-21 17:56:46 +0100

    commit integration latest

--- calculator/src/test/java/group13/calculator/IT.java
+++ calculator/src/test/java/group13/calculator/IT.java
@@ -16,5 +16,9 @@ public class IT {
 	public void test() {
 		assertEquals(24,calciint.add(calciint.add(8,8),calciint.add(4,4)));
 	}		
+	@Test
+	public void test1() {
+		assertEquals(14,calciint.add(calciint.add(5,5),calciint.add(2,2)));
+	}		
 	}
 
